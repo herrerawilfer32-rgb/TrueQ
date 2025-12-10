cb63a62dc597185830aad2f723790f9a38745d6b
BBDavidSS <brayand.santosa09@gmail.com>
2025-12-09 22:11:01 -0500

    doc AuthController.java
    se realizó docmentación de la clase

--- proyectoFinalPOO/src/controller/AuthController.java
+++ proyectoFinalPOO/src/controller/AuthController.java
@@ -1,8 +1,8 @@
-/*
+/**
  * Clase: AuthController
- * Autores: Anggel Leal, Wilfer Herrera, David Santos
- * Descripción: Controlador que maneja la autenticación de usuarios.
- * Centraliza la lógica de login y registro, delegando al UserService.
+ * Controlador que maneja la autenticación de usuarios, centraliza la lógica de login y registro, delegando al UserService.
+ * @author Anggel Leal, Wilfer Herrera, David Santos
+ * @version 1.2 
  */
 
 package controller;
@@ -13,20 +13,32 @@ import service.UserService;
 public class AuthController {
 
     private final UserService userService;
-
+    /**
+     * Constructor que recibe el servicio de usuarios, encargado de gestionar operaciones como inicio de sesión y registro.
+     */
     public AuthController(UserService userService) {
         this.userService = userService;
     }
 
-    // Maneja el inicio de sesión del usuario
+   /**
+     * Maneja el proceso de inicio de sesión del usuario.
+     * @param username nombre de usuario ingresado.
+     * @param password contraseña ingresada.
+     * @return el usuario autenticado si las credenciales son correctas, 
+     *         o null si el inicio de sesión falla.
+     */
     public User manejarLogin(String username, String password) {
         return userService.iniciarSesion(username, password);
     }
 
-    // Maneja el registro de un nuevo usuario en el sistema
+   /**
+     * Maneja el proceso de registro de nuevos usuarios, envía los datos al servicio para crear un nuevo usuario en el sistema.
+     * 
+     * @return true si el registro fue exitoso o false si hubo algún error
+     */
     public boolean manejarRegistro(String id, String username, String password, String correo, String nombre,
             String apellido, String ubicacion) {
         return userService.registrarUsuario(id, username, password, correo, nombre, apellido, ubicacion);
     }
 
-}
+}
