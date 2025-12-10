ca8b59a51db2990c7d44f9370a8b651ad98932c6
BBDavidSS <brayand.santosa09@gmail.com>
2025-12-09 22:04:46 -0500

    docAdminController.java
    Se realizó la documentación respectiva de la clase, encabezado(autores, versión) y comentarios de doc.

--- proyectoFinalPOO/src/controller/AdminController.java
+++ proyectoFinalPOO/src/controller/AdminController.java
@@ -1,7 +1,8 @@
-/*
+/**
  * Clase: AdminController
- * Autores: Anggel Leal, Wilfer Herrera, David Santos
- * DescripciÃ³n: Controlador de funciones administrativas.
+ * Controlador encargado de gestionar las acciones administrativas del sistema.Permite a un usuario administrador realizar operaciones como listar, buscar,actualizar y eliminar usuarios, publicaciones y ofertas, tambien cambiar roles y obtener estadísticas generales.
+ * @Author Anggel Leal, Wilfer Herrera, David Santos
+ * @Version 1
  */
 
 package controller;
@@ -17,19 +18,30 @@ import java.util.List;
 public class AdminController {
 
     private final AdminService adminService;
-
+    
+   /**
+     * Constructor que recibe el servicio encargado de la lógica administrativa.
+     */
+    
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
     }
-
+    
+   /**
+     * Devuelve una lista con todos los usuarios del sistema.
+     * El administrador puede usarla para ver o gestionar cuentas.
+     */
     public List<User> listarTodosLosUsuarios(String idAdmin) {
         return adminService.listarTodosLosUsuarios(idAdmin);
     }
-
+ 
     public User buscarUsuarioPorId(String idUsuario, String idAdmin) {
         return adminService.buscarUsuarioPorId(idUsuario, idAdmin);
     }
-
+    
+ /**
+     * Actualiza los datos de un usuario, devuelve true si la operación se hizo bien.
+     */
     public boolean actualizarUsuario(User usuario, String idAdmin) {
         try {
             return adminService.actualizarUsuario(usuario, idAdmin);
@@ -47,7 +59,9 @@ public class AdminController {
             return false;
         }
     }
-
+ /**
+     * Elimina un usuario del sistema,retorna true si el usuario fue eliminado correctamente.
+     */
     public boolean cambiarRolUsuario(String idUsuario, RolUsuario nuevoRol, String idAdmin) {
         try {
             return adminService.cambiarRolUsuario(idUsuario, nuevoRol, idAdmin);
@@ -56,7 +70,10 @@ public class AdminController {
             return false;
         }
     }
-
+    
+    /**
+     * Lista todas las publicaciones creadas en el sistema.
+     */
     public List<Publicacion> listarTodasLasPublicaciones(String idAdmin) {
         return adminService.listarTodasLasPublicaciones(idAdmin);
     }
@@ -69,6 +86,7 @@ public class AdminController {
             return false;
         }
     }
+    
 
     public List<Oferta> listarTodasLasOfertas(String idAdmin) {
         return adminService.listarTodasLasOfertas(idAdmin);
@@ -82,14 +100,22 @@ public class AdminController {
             return false;
         }
     }
-
+    
+    /**
+     * Cuenta cuántos usuarios hay registrados
+     */
     public int contarUsuarios(String idAdmin) {
         return adminService.contarUsuarios(idAdmin);
     }
-
+     /**
+     * Cuenta todas las publicaciones existentes
+     */
     public int contarPublicaciones(String idAdmin) {
         return adminService.contarPublicaciones(idAdmin);
     }
+     /**
+     * Cuenta todas las ofertas.
+     */
 
     public int contarOfertas(String idAdmin) {
         return adminService.contarOfertas(idAdmin);
