3faa0cbe4c05d8ac2295a41f576a0456c7a1d72d
BBDavidSS <brayand.santosa09@gmail.com>
2025-12-10 08:51:53 -0500

    doc EstadoOferta.java

--- proyectoFinalPOO/src/util/EstadoOferta.java
+++ proyectoFinalPOO/src/util/EstadoOferta.java
@@ -1,8 +1,9 @@
-/*
+/**
  * Clase: EstadoOferta
- * Autores: Anggel Leal, Wilfer Herrera, David Santos
- * DescripciÃ³n: Clase utilidad.
- */
+ *Representa los diferentes estados posibles de una oferta dentro del sistema de trueque o subastas.
+ * @author Anggel Leal, Wilfer Herrera, David Santos
+ * @version 1.2
+ */
 
 package util;
 
@@ -10,7 +11,6 @@ public enum EstadoOferta {
 	PENDIENTE, // La oferta ha sido realizada pero no ha sido aceptada ni rechazada
 	ACEPTADA, // La oferta ha sido aceptada por el vendedor
 	RECHAZADA, // La oferta ha sido rechazada por el vendedor
-	CANCELADA, // La oferta ha sido cancelada por el comprador antes de ser aceptada o
-				// rechazada
+	CANCELADA, // La oferta ha sido cancelada por el comprador antes de ser aceptada o rechazada
 	GANADORA // La oferta ha ganado una subasta
 }
